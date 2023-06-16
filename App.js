bd5df1d11f6bf2f4c6b18231b76304af69e28881
suyungshan <samsys2324@gmail.com>
2023-06-16 13:53:02 +0800

    刪除 polling
    刪除 polling 改使用 express 框架
    
    將 bot 的路徑與 setwebhook 的路徑放到 post 中
    
    刪除 express，只使用 bot.setWebhook
    
    修改 setWebhook 的網址(render+takon)和 post 的網址(token)
    
    加入import fetch from "node-fetch";
    
    安裝 node-fetch
    
    佈署到 Render 上因此不需要自訂埠號監聽
    
    加入埠號
    
    將 token 改為環境變數
    
    修改 new TelegramBot
    
    delet all console & comment
    
    加入 Render 的環境變數要件

--- App.js
+++ App.js
@@ -1,14 +1,22 @@
-//使用 import (ES6) 的方式導入 node-telegram-bot-api 模組
 import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+import fetch from "node-fetch";
 
-const bot = new TelegramBot("6072177457:AAHSnFucxpr3lBt4QX758s-bSK3m5b_n_CY", {
-  polling: true,
+const app = express();
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN);
+
+bot.setWebHook(
+  `https://workout-dngg.onrender.com/bot${process.env.TELEGRAM_BOT_TOKEN}`
+);
+
+app.use(express.json());
+
+app.post(`/bot${process.env.TELEGRAM_BOT_TOKEN}/`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
 bot.onText(/\/start/, (msg) => {
-  console.log(msg);
-  console.log(msg.chat);
-
   const chatId = msg.chat.id;
   const message =
     "歡迎使用健身機器人！請傳入您想鍛鍊的部位，例如：/exercise chest";
@@ -20,10 +28,10 @@ bot.onText(/\/exercise (.+)/, async (msg, match) => {
   const bodyPart = match[1];
   try {
     const response = await fetch(
-      "https://api.airtable.com/v0/appJeM7ssI2ScYGK0/Workout?maxRecords=12&view=Grid%20view",
+      `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Workout?maxRecords=12&view=Grid%20view`,
       {
         headers: {
-          Authorization: "Bearer keyyQYd2HGNFmM2gF",
+          Authorization: `Bearer ${process.env.AIRTABLE_API}`,
         },
       }
     );
@@ -32,19 +40,15 @@ bot.onText(/\/exercise (.+)/, async (msg, match) => {
     }
     const data = await response.json();
 
-    //將 data 的 records 陣列透過 filter 與用戶輸入的部位關鍵字做比對，取出對應的資料
     const matchingRecords = data.records.filter((record) => {
-      //將拉出的對應部位與擷取到的用戶輸入值都先轉成相同格式(這邊是全轉為小寫)再進行比對
       const exercisePart = record.fields.輸入部位.toLowerCase();
       return exercisePart === bodyPart.toLowerCase();
     });
 
-    //設定條件式，若部位不存在，則會呈現空陣列，因此若 matchingRecords 為空陣列的話，表示部位不匹配，藉此回傳錯誤訊息
     if (matchingRecords.length === 0) {
       const message = `很抱歉暫時沒有您希望的運動部位\n請輸入以下的部位名稱進行查詢\nShoulder\nArm\nBack\nLegs\nCore\nChest`;
       bot.sendMessage(chatId, message);
     } else {
-      //對應資料再透過 map 取出要回傳給 User 的資訊
       const matchingData = matchingRecords.map((data) => {
         return {
           id: data.id,
@@ -53,8 +57,7 @@ bot.onText(/\/exercise (.+)/, async (msg, match) => {
           輸入部位: data.fields.輸入部位,
         };
       });
-      console.log(matchingData);
-      //將透過 map 給 user 的資訊陣列透過 forEach 取出陣列中的每個物件，並提取我們要的物件資訊放入 message，再回傳給 Bot
+
       matchingData.forEach((exercise) => {
         const message = `根據您的選擇，推薦的運動項目為：${exercise.運動名稱}\n\n使用方式為：${exercise.描述}`;
         bot.sendMessage(chatId, message);
@@ -64,4 +67,8 @@ bot.onText(/\/exercise (.+)/, async (msg, match) => {
     console.error(error);
     bot.sendMessage(chatId, error.message);
   }
+});
+
+app.listen(3000, () => {
+  console.log("Express server is listening on port 3000");
 });
